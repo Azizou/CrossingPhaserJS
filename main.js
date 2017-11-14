cc29772e8b1ea87485e91abbeef828e2a584d91f
Azizou Ogbone <ogboneazizou@gmail.com>
2017-11-14 23:11:17 +0200

    refactoring, reducing magic number in code (aka use constants)

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var jumpButton;
 var spacefield;
 var spacedv;
 var requiredToWin = 4;
+var height = 600;
 // Create the state that will contain the whole game
 var mainState = {
     preload: function () {
@@ -27,8 +28,8 @@ var mainState = {
         this.cursor = game.input.keyboard.createCursorKeys();
 
         // Create the player in the middle of the game
-        this.player = game.add.sprite(180, 360+9*20, 'player');
-        this.winCount = 0;
+        this.player = game.add.sprite(190, height - 60, 'player');
+        this.winCount = 0; //reset on refresh, can cache this to avoid that.
 
         this.walls = game.add.group();
         //        this.coins = game.add.group();
@@ -50,32 +51,24 @@ var mainState = {
             ' eeee   eeee   eeee  ',
             'xxxxxxxxxxxxxxxxxxxx'
         ];
-
+        this.yOffset = height - 20*level.length - 60;
         for (var i = 0; i < level.length; i++) {
             for (var j = 0; j < level[i].length; j++) {
 
                 // Create a wall and add it to the 'walls' group
                 if (level[i][j] == 'x') {
-                    var wall = game.add.sprite(20 * j, 260 + 20 * i, 'wall');
+                    var wall = game.add.sprite(20 * j,  this.yOffset + 20 * i, 'wall');
                     this.walls.add(wall);
                 }
                 else if (level[i][j] == 'o') {
-                    var win = game.add.sprite(20 * j, 260 + 20 * i, 'win');
-                    this.wins.add(win);
+                    var win = game.add.sprite(20 * j, this.yOffset + 20 * i, 'win');
+                    this.wins.add(win);// can hold up to 20 wins
                 }
                 
-
-                //                // Create a coin and add it to the 'coins' group
-                //                  Could be implemented on higher levels
-                //                else if (level[i][j] == ' ') {
-                //                    var coin = game.add.sprite(64+64*j, 64+64*i, 'coin');
-                //                    this.coins.add(coin);
-                //                }
-
                 // Create a enemy and add it to the 'enemies' group
                 else if (level[i][j] == 'e') {
                     const initialX = (i % 4 == 0) ? 420 : -420;
-                    var enemy = game.add.sprite(-20 + 20 * j, 260 + 20 * i, 'enemy');
+                    var enemy = game.add.sprite(-20 + 20 * j, this.yOffset + 20 * i, 'enemy');
                     //                    enemy.body.velocity.x = 50;
                     enemy.speed = initialX;
                     this.trucks.add(enemy);
@@ -125,17 +118,22 @@ var mainState = {
             this.wins.getAt(this.winCount - 1).key = 'player';
             this.wins.getAt(this.winCount - 1).loadTexture('player',0);
             console.log(this.wins.getAt(this.winCount - 1)); 
-            player.body.x = 180;
-            player.body.y = 360+9*20;
+            player.body.x = 200;
+            player.body.y = height - 60 ;
         }
         else{
+            this.wins.getAt(this.winCount - 1).key = 'player';
+            this.wins.getAt(this.winCount - 1).loadTexture('player',0);
             console.log("You have won!");
+            // logic to go to next level after diplaying win
         }
     },
 
     // Function to restart the game
-    restart: function () {
-        game.state.start('main');
+    restart: function (player) {
+        // game.state.start('main');
+        player.body.x = 200;
+        player.body.y = height - 60 ;
     },
     loadLevel: function () {
     }
@@ -143,6 +141,6 @@ var mainState = {
 };
 
 // Initialize the game and start our state
-var game = new Phaser.Game(400, 600);
+var game = new Phaser.Game(400, 600,'gameContainer');
 game.state.add('main', mainState);
 game.state.start('main');
